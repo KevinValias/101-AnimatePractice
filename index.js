cdd0730dd2455b51d9841e843bffc5c826984783
Kevin Valias <Kevbodyworks@gmail.com>
2020-06-27 14:40:17 -0500

    Final commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let animationName = ""
 let elementColor = ""
+let elementMessage = ""
 
 // Can you use these two variables to change the type and text of the next element that's created?
 let elementType = ""
-let inputtedText = ""
+let elementSize = ""
 
 // function that handles the input "animation-name"
 const setAnimationName = (name) => {
@@ -15,19 +16,30 @@ const setAnimationName = (name) => {
 const setElementColor = (color) => {
   elementColor = color
 }
+const setElementSize = (element) => {
+  elementSize = element
+}
+
+// function that handles the input "element message"
+const setElementMessage = (message) => {
+  elementMessage = message
+}
+
 
 // This function creates a new element and attaches to the parent element: element-collector
 const createNewElement = () => {
   const elementContainer = document.getElementById("element-collector")
   
   // create a new element with default values
-  const newElement = document.createElement("H3")
-  const textNode = document.createTextNode("Hello")
+  
+  const newElement = document.createElement(elementSize)
+  const textNode = document.createTextNode(elementMessage)
 
   // Using dot.notation to access the new element's properties
   newElement.appendChild(textNode)
   newElement.classList.add("animated", "infinite", animationName)
   newElement.style.color = elementColor
+  newElement.style.fontSize = elementSize
 
   // attach the new element to the parent
   elementContainer.appendChild(newElement)
